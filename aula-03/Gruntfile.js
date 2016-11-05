bf245853283aa4dd34cec5752f3080abb84db12d
w.souza <wagner.junior@fh.com.br>
2016-11-05 21:59:01 -0200

    adicionar arquivos da aula 03

---
+++ aula-03/Gruntfile.js
@@ -0,0 +1,58 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        /* Copia os arquivos para o diretório 'dist' */
+        copy: {
+            public: {
+                expand: true,
+                cwd: 'public',
+                src: '**',
+                dest: 'dist'
+            }
+        },
+
+        clean: {
+            dist: {
+                src: 'dist'
+            }
+        },
+
+        useminPrepare: {
+            html: 'dist/**/*.html'
+        },
+
+        usemin: {
+            html: 'dist/**/*.html'
+        },
+
+        imagemin: {
+            public: {
+                expand: true,
+                cwd: 'dist/img',
+                src: '**/*.{png,jpg,gif}',
+                dest: 'dist/img'
+            }
+        }
+    });
+
+
+    //registrando task para minificação
+
+    grunt.registerTask('dist', ['clean', 'copy']);
+
+    grunt.registerTask('minifica', ['useminPrepare',
+        'concat', 'uglify', 'cssmin', 'usemin', 'imagemin'
+    ]);
+
+    // registrando tasks
+    grunt.registerTask('default', ['dist', 'minifica']);
+
+    // carregando tasks
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-usemin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+}
