d32e02d9bc0ae50fdee7f855383baa960dc856c8
w.souza <wagner.junior@fh.com.br>
2016-11-05 21:48:01 -0200

    adicionar scripts da segunda aula

--- aula-02/Gruntfile.js
+++ aula-02/Gruntfile.js
@@ -1,35 +1,48 @@
 module.exports = function(grunt) {
 
-   grunt.initConfig({
-      /* Copia os arquivos para o diretorio 'dist'*/
-      copy: {
-         public: {
-           expand: true,
-           cwd: 'public',
-           src: '**',
-           dest: 'dist'
-         }
-     },
+    grunt.initConfig({
+        /* Copia os arquivos para o diretório 'dist' */
+        copy: {
+            public: {
+                expand: true,
+                cwd: 'public',
+                src: '**',
+                dest: 'dist'
+            }
+        },
 
-     clean: {
-          dist: {
-              src: 'dist'
-          }
-     }
-  });
+        clean: {
+            dist: {
+                src: 'dist'
+            }
+        },
 
-  //registrando task para minificação
+        useminPrepare: {
+            html: 'dist/**/*.html'
+        },
 
-  grunt.registerTask('dist', ['clean', 'copy']);
+        usemin: {
+            html: 'dist/**/*.html'
+        }
+    });
 
-  // registrando tasks
-  grunt.registerTask('default', ['dist']);
 
-  // carregando tasks
-  grunt.loadNpmTasks('grunt-contrib-copy'); 
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-usemin');
+    //registrando task para minificação
+
+    grunt.registerTask('dist', ['clean', 'copy']);
+
+    grunt.registerTask('minifica', ['useminPrepare',
+        'concat', 'uglify', 'cssmin', 'usemin'
+    ]);
+
+    // registrando tasks
+    grunt.registerTask('default', ['dist', 'minifica']);
+
+    // carregando tasks
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-usemin');
 }
